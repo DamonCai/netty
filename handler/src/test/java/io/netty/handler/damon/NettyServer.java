d189605d38470254348f1925b0a684ac260c6b2a
damon <caidawen@yeah.net>
2024-05-07 18:21:46 +0800

    修改demo

--- handler/src/test/java/io/netty/handler/damon/NettyServer.java
+++ handler/src/test/java/io/netty/handler/damon/NettyServer.java
@@ -8,46 +8,53 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.handler.codec.bytes.ByteArrayEncoder;
-import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.Delimiters;
+import io.netty.handler.timeout.IdleStateHandler;
 
 /*
  * Netty的Client
  * 功能:向server发送控制台输入的消息，并接收server发回的消息并显示
  * */
 public class NettyServer {
-    public void start(int port) throws Exception {
-        ServerBootstrap strap = new ServerBootstrap();
-        //主线程
-        EventLoopGroup bossGroup = new NioEventLoopGroup();
-        //从线程
-        EventLoopGroup workerGroup = new NioEventLoopGroup();
-        try {
-            strap.group(bossGroup, workerGroup)
-                    //主线程监听通道
-                    .channel(NioServerSocketChannel.class)
-                    .option(ChannelOption.SO_BACKLOG, 1024)
-                    //定义从线程的handler链，责任链模式
-                    .childHandler(new ChannelInitializer<SocketChannel>() {
-                        @Override
-                        protected void initChannel(SocketChannel ch) {
-                            ch.pipeline().addLast("encoderByte", new ByteArrayEncoder());
-                            ch.pipeline().addLast("encoderStr", new StringEncoder());
-                            // 自定义handler
-                            ch.pipeline().addLast();
-                        }
-                    });
-            ChannelFuture future = strap.bind(port).sync();
-            future.channel().closeFuture().sync();
-        } finally {
-            bossGroup.shutdownGracefully();
-            workerGroup.shutdownGracefully();
-        }
+    public static void main(String[] args) throws InterruptedException {
+        // 首先，netty通过ServerBootstrap启动服务端
+        ServerBootstrap server = new ServerBootstrap();
+        EventLoopGroup parentGroup = new NioEventLoopGroup();
+        EventLoopGroup childGroup =new NioEventLoopGroup();
+        //第1步定义两个线程组，用来处理客户端通道的accept和读写事件
+        //parentGroup用来处理accept事件，childgroup用来处理通道的读写事件
+        //parentGroup获取客户端连接，连接接收到之后再将连接转发给childgroup去处理
+        server.group(parentGroup, childGroup);
 
+        //用于构造服务端套接字ServerSocket对象，标识当服务器请求处理线程全满时，用于临时存放已完成三次握手的请求的队列的最大长度。
+        //用来初始化服务端可连接队列
+        //服务端处理客户端连接请求是按顺序处理的，所以同一时间只能处理一个客户端连接，多个客户端来的时候，服务端将不能处理的客户端连接请求放在队列中等待处理，backlog参数指定了队列的大小。
+        server.option(ChannelOption.SO_BACKLOG, 128);
+
+        //第2步绑定服务端通道
+        server.channel(NioServerSocketChannel.class);
+
+        //第3步绑定handler，处理读写事件，ChannelInitializer是给通道初始化
+        server.childHandler(new ChannelInitializer<SocketChannel>() {
+            @Override
+            protected void initChannel(SocketChannel ch) throws Exception {
+                ch.pipeline().addLast(new IdleStateHandler(30, 30, 60));
+                //解码器，接收的数据进行解码，一定要加在SimpleServerHandler 的上面
+                //maxFrameLength表示这一贞最大的大小
+                //delimiter表示分隔符，我们需要先将分割符写入到ByteBuf中，然后当做参数传入；
+                //需要注意的是，netty并没有提供一个DelimiterBasedFrameDecoder对应的编码器实现(笔者没有找到)，因此在发送端需要自行编码添加分隔符，如 \r \n分隔符
+                ch.pipeline().addLast(new DelimiterBasedFrameDecoder(Integer.MAX_VALUE, Delimiters.lineDelimiter()[0]));
+                //把传过来的数据 转换成byteBuf
+                ch.pipeline().addLast(new SimpleServerHandler());
+            }
+        });
+
+        //第4步绑定8080端口
+        ChannelFuture future = server.bind(8080).sync();
+        //当通道关闭了，就继续往下走
+        future.channel().closeFuture().sync();
     }
 
-    public static void main(String[] args) throws Exception {
-        System.out.println("start server");
-        new NettyServer().start(8000);
-    }
+
 }
